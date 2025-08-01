5680950feb6ae2b4effdfa97f24cda63dacc4433
SantiNones <santiagonones@gmail.com>
2025-08-01 09:00:06 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-import "bootstrap";
-import "./style.css";
+function newCardGenerator() {
+  const number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10" , "J", "Q", "K"];
+  const suit = ["♥", "♦", "♠", "♣"]; 
+
+  const randomNumber = number[Math.floor(Math.random() * number.length)];
+  const randomSuit = suit[Math.floor(Math.random() * suit.length)];
+
+  document.querySelector(".topsuit p").innerText = randomSuit;
+  document.querySelector(".number p").innerText = randomNumber;
+  document.querySelector(".bottomsuit p").innerText = randomSuit; 
+
+  const card = document.querySelector(".card");
+  card.classList.remove("heart", "diamond", "spade", "club");
+
+  if (randomSuit === "♥") card.classList.add("heart");
+    else if (randomSuit === "♦") card.classList.add("diamond");
+    else if (randomSuit === "♠") card.classList.add ("spade");
+    else if (randomSuit === "♣") card.classList.add ("club")
+  }
+
+window.onload = newCardGenerator; 
+
+document.querySelector(".changeCardButton").addEventListener("click", newCardGenerator);
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
